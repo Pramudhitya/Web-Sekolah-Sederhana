2db4360cfa1e51bc5e1e2c531aeebb6d01c58ea5
Pramudhitya <kacanglupakan80@gmail.com>
2025-11-29 13:58:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Hamburger Menu
+var navlinks = document.getElementById('navlinks');
+function showmenu() {
+  navlinks.style.right = '0';
+  navlinks.style.display = 'block';
+}
+function hidemenu() {
+  navlinks.style.right = '-200px';
+  navlinks.style.display = 'none';
+}
+
+
+// Animasi Scroll
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
